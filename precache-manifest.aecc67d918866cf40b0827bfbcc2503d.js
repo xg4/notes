d4fd256a84a1af45b120b30a8644474dd73315ee
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-24 03:14:44 +0000

    Deploy xg4/notes to github.com/xg4/notes.git:gh-pages

---
+++ precache-manifest.aecc67d918866cf40b0827bfbcc2503d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0dc0b057c8987417638",
+    "url": "js/about.c0d03a2a.js"
+  },
+  {
+    "revision": "95f4fb7c9966e9c5e451",
+    "url": "css/app.040b19ea.css"
+  },
+  {
+    "revision": "95f4fb7c9966e9c5e451",
+    "url": "js/app.0fbcfbb9.js"
+  },
+  {
+    "revision": "f589e81ad1c3f47f0d56",
+    "url": "js/chunk-vendors.26d766a4.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "bd7b730289f5cc83bdab216493b46974",
+    "url": "index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+];
