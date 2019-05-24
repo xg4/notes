2e0dbf3c17d32da482733803e684a7d6c5c42159
xg4 <xingor4@gmail.com>
2019-05-24 10:55:47 +0800

    chore(deps): add dependencies

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,16 @@
 module.exports = {
   plugins: {
-    autoprefixer: {}
+    'postcss-preset-env': {},
+    'postcss-flexbugs-fixes': {},
+    'postcss-px-to-viewport': {
+      unitToConvert: 'px',
+      viewportWidth: 375,
+      unitPrecision: 5,
+      viewportUnit: 'vw',
+      fontViewportUnit: 'vw',
+      selectorBlackList: ['postcss-'],
+      minPixelValue: 2,
+      mediaQuery: false
+    }
   }
-};
+}
