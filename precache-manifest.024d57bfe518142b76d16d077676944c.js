dd5f7ec86c22344f0d42afe4952f813600489f57
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-28 03:11:16 +0000

    Deploy xg4/notes to github.com/xg4/notes.git:gh-pages

---
+++ precache-manifest.024d57bfe518142b76d16d077676944c.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f7a0fe884fdf6f0a619",
+    "url": "css/404.33ae7521.css"
+  },
+  {
+    "revision": "4f7a0fe884fdf6f0a619",
+    "url": "js/404.34f99d3f.js"
+  },
+  {
+    "revision": "17e4d84e34812c1619ef",
+    "url": "css/app.e251bf98.css"
+  },
+  {
+    "revision": "17e4d84e34812c1619ef",
+    "url": "js/app.f5920b2d.js"
+  },
+  {
+    "revision": "335d258d543d721f75ad",
+    "url": "css/chunk-vendors.36ac53e1.css"
+  },
+  {
+    "revision": "335d258d543d721f75ad",
+    "url": "js/chunk-vendors.6cd7b581.js"
+  },
+  {
+    "revision": "451827a521cb5c6488c4",
+    "url": "css/createAndEdit.2f66fe40.css"
+  },
+  {
+    "revision": "451827a521cb5c6488c4",
+    "url": "js/createAndEdit.04baa241.js"
+  },
+  {
+    "revision": "9bbfac03fe4cb6fa8d1b",
+    "url": "css/detail.9139d1dd.css"
+  },
+  {
+    "revision": "9bbfac03fe4cb6fa8d1b",
+    "url": "js/detail.25334eae.js"
+  },
+  {
+    "revision": "9bbab32faf17b858ea7b",
+    "url": "css/home.79dee3bd.css"
+  },
+  {
+    "revision": "9bbab32faf17b858ea7b",
+    "url": "js/home.3ed4994a.js"
+  },
+  {
+    "revision": "b6725aebd4d240866baa",
+    "url": "css/home~star.58b48898.css"
+  },
+  {
+    "revision": "b6725aebd4d240866baa",
+    "url": "js/home~star.3801a98d.js"
+  },
+  {
+    "revision": "44f9a2f92773b1170776",
+    "url": "css/profile.fb71dba5.css"
+  },
+  {
+    "revision": "44f9a2f92773b1170776",
+    "url": "js/profile.e4cfb562.js"
+  },
+  {
+    "revision": "7f799175f17aba3870bb",
+    "url": "js/star.4daac3d6.js"
+  },
+  {
+    "revision": "316ec3cfeeaf66b0a4ab312a6f206bd2",
+    "url": "index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.356dedcadfa1c6ecf85979bab661e670.js"
+  "precache-manifest.024d57bfe518142b76d16d077676944c.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "notes"});
