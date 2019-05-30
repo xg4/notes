a47aefb1659b68ac24cc76d04efa358d7f1e51c7
xg4 <xingor4@gmail.com>
2019-05-30 14:57:38 +0800

    refactor: rename config

--- src/store/types.js
+++ src/store/types.js
@@ -16,7 +16,6 @@ export const DELETE_NOTES = 'DELETE_NOTES'
 export const DELETE_COMPLETED_NOTES = 'DELETE_COMPLETED_NOTES'
 
 // mutation
-
 export const PUT_NOTES = 'PUT_NOTES'
 
 export const PUT_TAGS = 'PUT_TAGS'
