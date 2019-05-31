96c373cedf1a9d7bbb1950fe1dec25c75aa9b0e9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-31 05:31:09 +0000

    Deploy xg4/notes to github.com/xg4/notes.git:gh-pages

---
+++ precache-manifest.7963ce82700048e4247b617e817695df.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f7a0fe884fdf6f0a619",
+    "url": "css/404.33ae7521.css"
+  },
+  {
+    "revision": "4f7a0fe884fdf6f0a619",
+    "url": "js/404.34f99d3f.js"
+  },
+  {
+    "revision": "34762a2167c9dcefbc67",
+    "url": "css/app.157df666.css"
+  },
+  {
+    "revision": "34762a2167c9dcefbc67",
+    "url": "js/app.ee77feaa.js"
+  },
+  {
+    "revision": "6ce774dda2f9576fa7d3",
+    "url": "css/chunk-vendors.a55f598f.css"
+  },
+  {
+    "revision": "6ce774dda2f9576fa7d3",
+    "url": "js/chunk-vendors.4c26f3eb.js"
+  },
+  {
+    "revision": "a03e0e22357a921c240a",
+    "url": "css/new.1dc44791.css"
+  },
+  {
+    "revision": "a03e0e22357a921c240a",
+    "url": "js/new.ee5454c8.js"
+  },
+  {
+    "revision": "0b3ec0eb18b4efe5f203",
+    "url": "css/note.6a40cbf0.css"
+  },
+  {
+    "revision": "0b3ec0eb18b4efe5f203",
+    "url": "js/note.994a3a4d.js"
+  },
+  {
+    "revision": "765b05ebfa590473cb54",
+    "url": "css/notes.b1ae3862.css"
+  },
+  {
+    "revision": "765b05ebfa590473cb54",
+    "url": "js/notes.bb89372d.js"
+  },
+  {
+    "revision": "3f69a2b7720d691d3194",
+    "url": "css/profile.a9f54981.css"
+  },
+  {
+    "revision": "3f69a2b7720d691d3194",
+    "url": "js/profile.45384284.js"
+  },
+  {
+    "revision": "c24a2fccc2dbb88b688d172772c527b2",
+    "url": "index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.516ad6d09c333c65ef1db7b42b32a286.js"
+  "precache-manifest.7963ce82700048e4247b617e817695df.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "notes"});
